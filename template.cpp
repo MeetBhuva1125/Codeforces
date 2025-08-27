1b42b25e6a77000ab073d297263e02cbdac4687d
MeetBhuva1125 <meetpatel0852@gmail.com>
2025-08-27 23:08:39 +0530

    template change

--- template.cpp
+++ template.cpp
@@ -5,6 +5,12 @@ typedef long long ll;
 typedef double dd;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
+typedef unordered_map<int,int> umpii;
+typedef unordered_map<long long, long long> umpll;
+typedef vector<int> v1i;
+typedef vector<long long> v1l;
+typedef vector<vector<int>> v2i;
+typedef vector<vector<long>> v2l;
 
 // Functions as alternatives to macros (more type-safe)
 template<typename T>
@@ -12,13 +18,13 @@ vector<T> v1d_t(int n) { return vector<T>(n,0); }
 template<typename T>
 vector<vector<T>> v2d_t(int m, int n) { return vector<vector<T>>(m, vector<T>(n,0)); }
 
-#define forloop(i, a, b) for (int i=a; i<b; i++)
+#define forloop(i, a, b) for(int i=a; i<b; i++)
 #define forloopR(i, a, b) for(int i=a; i>=b; i--)
-#define v1d(type, name, n) vector<type> name(n)
-#define v2d(type, name, m, n) vector<vector<type>> name(m, vector<type>(n))
+// #define v1d(type, name, n) vector<type> name(n,0)
+// #define v2d(type, name, m, n) vector<vector<type>> name(m, vector<type>(n,0))
 
 void solve(){
-    
+
 }
 
 int main(){
